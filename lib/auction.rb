5f160dce7cfa71675e81a813ed677891840d26c9
Justin Lee <140270202+JustinSteel@users.noreply.github.com>
2023-09-26 11:28:09 -0600

    add to iteration 3

--- lib/auction.rb
+++ lib/auction.rb
@@ -20,4 +20,50 @@ class Auction
   def potential_revenue
     @items.map { |item| item.current_high_bid.to_i }.sum
   end
+
+  def bidders
+    @items.map do |item|
+      item.bids.keys.flat_map do |attendee|
+        # require 'pry'; binding.pry
+        if attendee.name != nil
+          attendee.name
+        end
+      end
+    end.compact.flatten.uniq
+  end
+
+  
+  
+  def items_bid_on
+    all_items = [] 
+    
+    @items.map do |item|
+      item.bids.keys.flat_map do |attendee|
+        if !unpopular_items.include?(item)
+          all_items << item
+        end
+      end
+      # require 'pry'; binding.pry
+      all_items.uniq
+    end
+  end
+  # we will be returning a hash
+      #Each key will be an attendee object (must be unique)
+      # Each value is a hash
+      #quantity: total quantity of the item at the market
+      #venors: al the venors that sell this item
+
+  def bidder_info
+    attendee_info = {}
+
+    @items.each do |item|
+      item.bids.each do |attendee, bid|
+        attendee_info[attendee] = {
+          budget: attendee.budget,
+          items: items_bid_on
+      }
+      end
+      attendee_info
+    end
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -115,4 +115,52 @@ RSpec.describe Auction do
       expect(@auction.potential_revenue).to eq(87)
     end
   end
+
+  describe '#bidders' do
+    it 'returns an array of bidder names' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+    expect(@auction.bidders.sort).to eq(["Bob", "Megan", "Mike"])  
+    end
+
+    it 'returns a hash of bidder info' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expected = {
+        @attendee1 => 
+        {
+          :budget => 50,
+          :items => [@item1]
+        },
+        @attendee2 => {
+          :budget => 75,
+          :items => [@item1, @item2]
+        },
+        @attendee3 => {
+          :budget => 100,
+          :items => [@item1]
+        },
+        }
+        
+        expect(@auction.bidder_info).to eq(expected)
+    end
+  end
 end
